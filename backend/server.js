57d89dc5661b2bf22d34f38bb883b5e1d1d5a01c
Sumit <sumithjadhav99@gmail.com>
2023-03-29 16:45:57 +0530

    Add routes and controller

--- backend/server.js
+++ backend/server.js
@@ -8,4 +8,7 @@ app.get('/', (req, res)=> {
     res.status(200).json({message: 'Welcome to support desk API'})
 })
 
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
