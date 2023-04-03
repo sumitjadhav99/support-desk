56a7c61bde8587b9d008f7f8d49d6c1096ecc354
Sumit <sumithjadhav99@gmail.com>
2023-04-03 17:19:00 +0530

    Fetch tickets from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import NewTicket from './pages/NewTicket'
+import Tickets from './pages/Tickets';
 
 function App() {
   return <>
@@ -19,6 +20,9 @@ function App() {
         <Route path='/register' element={<Register/>}/>
         <Route path='/new-ticket' element={<PrivateRoute />}>
               <Route path='/new-ticket' element={<NewTicket />} />
+            </Route>
+        <Route path='/tickets' element={<PrivateRoute />}>
+              <Route path='/tickets' element={<Tickets />} />
             </Route>
       </Routes>
     </div>
